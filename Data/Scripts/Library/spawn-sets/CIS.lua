0b94417f2edc6e6ae2fde7331623b89d4a64c4af
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-04-27 17:34:10 +0200

    .

---
+++ Data/Scripts/Library/spawn-sets/CIS.lua
@@ -0,0 +1,58 @@
+return {
+	Space_Unit_Table = {
+		{"DH_Omni", 100}
+		,{"Recusant_CIS", 100}
+		,{"Munificent_C3_CIS", 100}
+		,{"Munificent_CIS", 100}
+		,{"Munifex", 100}
+		,{"C9979_Carrier", 100}
+		,{"Hardcell", 100}
+		,{"Diamond_Frigate", 100}
+	},
+	-- Land_Unit_Table = {
+	-- 	{"Imperial_Army_Trooper_Company", 4}
+	-- 	,{"Imperial_Army_Guard_Company", 3}
+	-- 	,{"Imperial_Navy_Trooper_Company", 1}
+	-- 	,{"Imperial_Stormtrooper_Company", 2}
+	-- 	,{"Destroyer_Droid_I_W_Company", 0.5}
+	-- 	,{"Imperial_Dwarf_Spider_Droid_Company", 2, StartYear = 12}
+	-- 	,{"Shadow_Stormtrooper_Company", 0.3}
+	-- 	,{"Imperial_Jumptrooper_Company", 1}
+	-- 	,{"Imperial_Scout_Trooper_Company", 1}
+	-- 	,{"Imperial_74Z_Bike_Company", 2}
+	-- 	,{"AT_ST_Company", 3}
+	-- 	,{"AT_ST_A_Company", 3, StartYear = 10}
+	-- 	,{"Chariot_LAV_Company", 3}
+	-- 	,{"S_1_Firehawke_Company", 2}
+	-- 	,{"PX4_Company", 1}
+	-- 	,{"Imperial_AT_AT_Walker_Company", 2}
+	-- 	,{"Imperial_AT_AT_Walker_Turbolaser_Refit_Company", 3, StartYear = 10}
+	-- 	,{"Imperial_Dropship_Transport_Company", 1}
+	-- 	,{"Deathhawk_Company", 1}
+	-- 	,{"SPMAG_Walker_Company", 1}
+	-- 	,{"SD_9_Droid_Company", 0.1, StartYear = 9}
+	-- },
+	Groundbase_Table = {
+		"E_Ground_Barracks",
+		"E_Ground_Barracks",
+		"E_Ground_Light_Vehicle_Factory",
+		"E_Ground_Heavy_Vehicle_Factory",
+		"E_Ground_Advanced_Vehicle_Factory",
+	},
+	Starbase_Table = {
+		"Empire_Star_Base_1",
+		"Empire_Star_Base_2",
+		"Empire_Star_Base_3",
+		"Empire_Star_Base_4",
+		"Empire_Star_Base_5",
+	},
+	Shipyard_Table = {
+		"Empire_Shipyard_Level_One",
+		"Empire_Shipyard_Level_Two",
+		"Empire_Shipyard_Level_Three",
+		"Empire_Shipyard_Level_Four",
+	},
+	Defenses_Table = require("spawn-sets/GOLANS"),
+	Government_Building = "Empire_Office",
+	GTS_Building = "Ground_Hypervelocity_Gun"
+}
